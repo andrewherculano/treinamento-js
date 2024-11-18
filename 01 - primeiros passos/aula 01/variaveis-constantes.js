5f07d4a3b89147fe66689e3822f276dd5f805276
andrewherculano <andrewherculano@hotmail.com>
2024-11-18 07:26:47 -0300

    resolução exercicios 01 - constantes e variaveis

---
+++ 01 - primeiros passos/aula 01/variaveis-constantes.js
@@ -0,0 +1,14 @@
+let age = 23
+let currentYear = 2024
+console.log(age, currentYear)
+
+const randomNumber = 8129
+console.log(randomNumber)
+
+/*
+váriaveis e contanstes são utilizadas para armazenar um valor em memória que
+posteriormente serão utilizados.
+para armazenar um valor que poderá ter alterações o indicado é sempre o uso da
+palavra reservada "let", já para armazenar valores que são constantes ou seja,
+não sofreram alterações é utilizado o uso da palavra reservada "const".
+*/
