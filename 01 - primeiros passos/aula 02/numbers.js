6d98437086801aba9a0e0341e9c664b79dd4d1c5
andrewherculano <andrewherculano@hotmail.com>
2024-11-19 20:12:53 -0300

    numbers

---
+++ 01 - primeiros passos/aula 02/numbers.js
@@ -0,0 +1,17 @@
+// inteiros e decimais
+const radius = 10
+const pi = 3.14
+
+// operadores aritimeticos
+const reminder = 11 % 2 // resto da divisão entre os dois operandos
+console.log(reminder)
+
+const area = pi * radius ** 2
+console.log(area)
+
+/* ordem das operações
+1- parenteses ()
+2- expoentes ou raizes
+3- multiplicação e divisão
+4- adição e subtração
+*/
