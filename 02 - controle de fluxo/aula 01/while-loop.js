ccfcebff34ca90f7d00302388133092d412ee7c8
andrewherculano <andrewherculano@hotmail.com>
2025-06-24 20:18:19 -0300

    while loop

---
+++ 02 - controle de fluxo/aula 01/while-loop.js
@@ -0,0 +1,15 @@
+// while loop
+let i = 0
+
+while (i < 5) {
+    console.log(`dentro do loop: ${i}`)
+    i++
+}
+
+const names = ['Andrew', 'Herculano', 'Silva']
+let index = 0
+
+while (index < names.length) {
+    console.log(names[index])
+    index++
+}
