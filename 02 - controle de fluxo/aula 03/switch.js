05a2ff7a7ed1767f93173bf64bfb693c6d2c014f
andrewherculano <andrewherculano@hotmail.com>
2025-06-28 23:25:58 -0300

    switch

---
+++ 02 - controle de fluxo/aula 03/switch.js
@@ -0,0 +1,18 @@
+const grade = 'C'
+
+switch (grade) {
+  case 'A':
+    console.log('você tirou A')
+    break
+  case 'B':
+    console.log('você tirou B')
+    break
+  case 'C':
+    console.log('você tirou C')
+    break
+  case 'D':
+    console.log('você tirou D')
+    break
+  default:
+    console.log('nota inválida')
+}
